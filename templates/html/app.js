7ee66aca69e98e27adc6b322443fd4b1269f3caf
jul lo <lo.juliane@gmail.com>
2023-10-07 17:41:01 -0400

    modified app.js and index.html in template/html folder.

--- templates/html/app.js
+++ templates/html/app.js
@@ -4,6 +4,41 @@ let queryUrl = "http://127.0.0.1:5000/";
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
-  console.log(data);
+  console.log(data[0][0]);
 });
+
+// Set up dropdown menu
+function init(){d3.json(queryUrl).then(function(data) {
+  let dropdownMenu = d3.select("#selDataset");
+  for (let j = 0; j < data.length; j++) {
+    
+      dropdownMenu.append("option").text(data[j][0].location.city).property("value", data[j][0].location.city)};
+  
+chart(data[0][0].location.city);
+  
+})}
+
+function optionChanged(op){
+  chart(op)
+}
+init();
+
+function chart(cityID){d3.json(queryUrl).then(function (data){
+  let cityData = data
+  result = cityData.filter(cityID[0].location.city)
+  console.log(result);
+  //   let sample_values = result.sample_values
+  //   let otu_ids = result.otu_ids
+})}
+
+
+
+
+
+
+
+
+
+
+
 
