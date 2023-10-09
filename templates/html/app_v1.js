e4bcf37e414238549d3e7a3ea66cca3a33cebf17
jul lo <lo.juliane@gmail.com>
2023-10-09 13:24:34 -0400

    extra files for tests.

---
+++ templates/html/app_v1.js
@@ -0,0 +1,42 @@
+// Set up first chart
+const ctx1 = document.getElementById('myChart1');
+      
+        new Chart(ctx1, {
+          type: 'bar',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+
+    const ctx2 = document.getElementById('myChart2');
+      
+        new Chart(ctx2, {
+          type: 'bar',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
