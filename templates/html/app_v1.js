85fc6e3ff71d335550f0ef0be2fc83810bb6628d
jul lo <lo.juliane@gmail.com>
2023-10-11 18:23:44 -0400

    modified script.py to incorporate hotel data.

--- templates/html/app_v1.js
+++ templates/html/app_v1.js
@@ -1,10 +1,16 @@
+// Perform url call to local host
+let url = 'http://localhost:5000/';
+
+// d3 json read data from localhost
+d3.json(url).then(function(data){
+
 // Set up first chart
-const ctx1 = document.getElementById('myChart1');
+const ctx1 = document.getElementById("category-selector");
       
         new Chart(ctx1, {
           type: 'bar',
           data: {
-            labels: ['New York City', 'Los Angeles', 'Phoenix', 'Chicago', 'San Antonio', 'San Diego', 'Dallas', 'Philadelphia' , 'Houston' ,'Austin'],
+            labels: [cityCoordinates.key],
             datasets: [{
               label: 'Hotels Ratings',
               data: [5,4,4.5,4,4,4,4,4,4,4,4],
@@ -46,3 +52,6 @@ const ctx1 = document.getElementById('myChart1');
             }
           }
         });
+
+})
+    
